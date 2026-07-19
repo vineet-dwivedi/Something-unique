19ffdd40a18d77ffa0d10c676fda9d3858f88b1f
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-07-19 18:50:51 +0530

    Api addon in router server

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -5,6 +5,14 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 app.use(morgan("combined"));
 
+app.get("/api/router/health", (req, res) => {
+    res.status(200).json({ message: "Router server is healthy!" });
+});
+
+app.get("/api/router/ready", (req, res) => {
+    res.status(200).json({ message: "Router server is ready!" });
+});
+
 app.use((req,res,next)=>{
     const headers = req.headers.host;
     const sandboxId = headers.split(".")[0];
