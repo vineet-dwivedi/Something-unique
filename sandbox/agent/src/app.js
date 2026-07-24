f18c051359c96c119399abd83517ebd55f5bf8b6
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-07-24 13:42:20 +0530

    Read files api addon in agent

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -25,4 +25,36 @@ app.get("/list-files",async (req,res) => {
     })
 })
 
+app.get("read-files",async (req,res)=>{
+    const files = req.query.files;
+
+    if(!files) {
+        return res.status(400).json({
+            message: 'No files specified in query param',
+            status: 'error'
+        })
+    }
+
+    const fileList = files.split(',');
+
+    const results = await Promise.all(fileList.map(async (file) => {
+        const filePath = `${WORKING_DIR}/$(file)`;
+        try{
+            const content = await fs.promises.readFile(filePath, 'utf-8');
+            return {
+                [filePath]: content,
+            }
+        } catch (err){
+            return {
+                [filePath]: `Error reading file: ${err.message}`,
+            }
+        }
+    }));
+
+    res.status(200).json({
+        message: 'File contents',
+        files: results
+    })
+})
+
 export default app;
